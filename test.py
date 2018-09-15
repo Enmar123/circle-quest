2006f749820cbfa47aaea1b567c80e6ef90f077b
Enmar123 <js_enmar1@hotmail.com>
2018-09-15 18:59:34 -0400

    imu test?

--- test.py
+++ test.py
@@ -4,11 +4,13 @@ Created on Sat Sep 15 00:37:19 2018
 
 @author: Jason
 """
-
+from matrix_io.proto.malos.v1 import driver_pb2 # MATRIX Protocol Buffer driver library
+from matrix_io.proto.malos.v1 import sense_pb2 # MATRIX Protocol Buffer sensor library
 from imu import imu_data_callback
 from time import sleep
 
 
 while True:
-    print(imu_data_callback())
+    data = sense_pb2.Imu().FromString(data[0])
+    print(imu_data_callback(data))
     sleep(1)
