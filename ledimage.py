b5515bd25482a2db8f2802f1c2ecfd29323e378c
nick <npalermo10@gmail.com>
2018-09-15 12:49:17 -0400

    no floats

--- ledimage.py
+++ ledimage.py
@@ -6,15 +6,15 @@ class ImageCreator():
         self.out = np.zeros([35,4]) 
 
     def set_led(self, index, r,g,b,w):
-        self.out[index] = [r,g,b,w]
+        self.out[index] = [int(r),int(g),int(b),int(w)]
 
     def clear_led(self, index):
         self.out[index] = [0,0,0,0]
 
     def brighten_led(self, index, intensity):
-        img.out[index][img.out[index] >0 ] += intensity 
+        img.out[index][img.out[index] >0 ] += int(intensity )
 
     def dim_led(self, index, intensity):
-        img.out[index][img.out[index] >0 ] -= intensity 
+        img.out[index][img.out[index] >0 ] -= int(intensity )
         
 
